2a6508495b3d511bcd14860bfd7feb94054956d9
eityamo <eityamo@gmail.com>
2026-05-15 13:35:25 +0900

    修正

--- src/lib/members.ts
+++ src/lib/members.ts
@@ -13,15 +13,15 @@ export const members: Member[] = [
   {
     name: "森本敬一",
     role: "復興プロジェクト推進",
-    title: "プロジェクトマネージャー",
+    title: "ガソリンスタンド経営",
     comment: "能登の地に再び灯りをともすために、一歩ずつ前に進んでいます。",
     image: `${basePath}/images/members/member001.jpg`,
     color: "#ff0000",
   },
   {
     name: "行廣映二",
-    role: "コーディネーター",
-    title: "ホームページ担当",
+    role: "ホームページ制作",
+    title: "Webエンジニア",
     comment: "パソコンやスマホが苦手なみなさんのサポートをさせていただいてます。",
     image: `${basePath}/images/members/member000.jpg`,
     color: "#008000",
